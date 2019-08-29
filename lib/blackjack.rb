3ed4f309af229e68953e7771d39186964bc9cc65
Shaqqour M <mshaqqourm@gmail.com>
2019-08-29 23:26:02 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,66 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  sum = deal_card + deal_card
+  display_card_total(sum)
+  sum
 end
 
-def hit?
-  # code hit? here
+def hit?(total)
+  prompt_user
+  input = get_user_input
+  until input == 's' || input == 'h'
+    invalid_command
+    prompt_user
+    input = get_user_input
+  end
+  if input == 'h'
+   total += deal_card
+  end
+  total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
-
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
 
 def runner
-  # code runner here
+  system "clear"
+  welcome
+  total = initial_round
+  until total >= 21
+    temp = hit?(total)
+    if temp == total
+      break
+    end
+    total = temp
+    system "clear"
+    sleep 0.2
+    display_card_total(total)
+  end
+  if total > 21
+    end_game(total)
+  end
 end
-    
